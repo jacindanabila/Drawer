be11c4771ad3b91ff96200ac555e6fad7f5b2569
jacindanabila <100058846+jacindanabila@users.noreply.github.com>
2022-10-08 00:20:19 +0700

    Add files via upload
    Workshop Mobile Acara 20

---
+++ settings.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Settings",
+        style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+        ), 
+      ),
+    );
+  }
+}
