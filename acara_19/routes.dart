0b4ab0d65ed21735abfb528ad02e6b9a4cc30240
jacindanabila <100058846+jacindanabila@users.noreply.github.com>
2022-10-08 11:08:15 +0700

    Add files via upload

---
+++ acara_19/routes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:route_generator/screen.dart';
+
+class RouteGenerator{
+  static Route<dynamic> generatorRoute(RouteSettings settings){
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/about':
+        return MaterialPageRoute(builder: (_) => AboutPage());
+      default:
+      return _errorRoute();
+    }
+  }
+  static Route<dynamic> _errorRoute(){
+    return MaterialPageRoute(builder: (_){
+    return Scaffold(
+      appBar: AppBar(title: Text("Error")),
+      body: Center(child: Text('Error Page')),
+      );
+    });
+  }
+}
