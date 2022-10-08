4afffc11f28f25ae155a7e6a022d0b0357630a25
jacindanabila <100058846+jacindanabila@users.noreply.github.com>
2022-10-08 10:58:34 +0700

    Add files via upload

---
+++ acara_20/home.dart
@@ -0,0 +1,19 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+      return Container(
+        child: Center(
+          child: Text("Home", 
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+          ),
+        ),
+      );
+  }
+}
---
+++ acara_20/profil.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+      return Container(
+        child: Center(
+          child: Text("Profile",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+          ),
+        ),
+      );
+   }
+}
